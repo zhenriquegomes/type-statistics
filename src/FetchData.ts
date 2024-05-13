4de1f8d4ebcfea61f736a407667d3533526fbdd2
zhenriquegomes <henriquegomesnunes19@gmail.com>
2024-05-13 06:12:44 -0300

    feat: improve fetchData error handling

--- src/FetchData.ts
+++ src/FetchData.ts
@@ -1,5 +1,14 @@
-async function fetchData<T>(): Promise<T> {
-  const response = await fetch("https://api.origamid.dev/json/transacoes.json");
-  const json = await response.json();
-  return json;
+async function fetchData<T>(url: string): Promise<T | null> {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+    const json = await response.json();
+    return json;
+  } catch {
+    return null;
+  }
 }
+
+export default fetchData;
