c9ab2c762e4243a5491bc288576b223fcb1ffbe2
zhenriquegomes <henriquegomesnunes19@gmail.com>
2024-05-14 06:02:53 -0300

    feat: add Statistics interface

---
+++ src/types/Statistics.d.ts
@@ -0,0 +1,10 @@
+interface Statistics {
+  total: number;
+  creditCard: number;
+  billet: number;
+  paid: number;
+  refusedByCreditCard: number;
+  awaitingPayment: number;
+  reversed: number;
+  bestSellDay: number;
+}
