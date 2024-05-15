2e64e1f1eb62c8cb4010e71b2d29a025f5ec9f53
zhenriquegomes <henriquegomesnunes19@gmail.com>
2024-05-15 06:25:57 -0300

    feat: calculate best sales day

--- src/CalculateStatistics.ts
+++ src/CalculateStatistics.ts
@@ -6,7 +6,7 @@ function calculateTotal(transactions: Transaction[]): number {
   }, 0);
 }
 
-function calculateByFilter<T>(
+function countByFilter<T>(
   transactions: Transaction[],
   property: keyof Transaction,
   value: T
@@ -20,40 +20,83 @@ function calculateByFilter<T>(
   }, 0);
 }
 
+function getBestSalesDay(transactions: Transaction[]): string {
+  const week = {
+    ["Domingo"]: 0,
+    ["Segunda"]: 0,
+    ["Terça"]: 0,
+    ["Quarta"]: 0,
+    ["Quinta"]: 0,
+    ["Sexta"]: 0,
+    ["Sabado"]: 0,
+  };
+  for (let i = 0; i < transactions.length; i++) {
+    if (transactions[i].data.getDay() === 0) {
+      week["Domingo"] += 1;
+      continue;
+    }
+    if (transactions[i].data.getDay() === 1) {
+      week["Segunda"] += 1;
+      continue;
+    }
+    if (transactions[i].data.getDay() === 2) {
+      week["Terça"] += 1;
+      continue;
+    }
+    if (transactions[i].data.getDay() === 3) {
+      week["Quarta"] += 1;
+      continue;
+    }
+    if (transactions[i].data.getDay() === 4) {
+      week["Quinta"] += 1;
+      continue;
+    }
+    if (transactions[i].data.getDay() === 5) {
+      week["Sexta"] += 1;
+      continue;
+    }
+    if (transactions[i].data.getDay() === 6) {
+      week["Sabado"] += 1;
+      continue;
+    }
+  }
+  console.log(week);
+  const [bestDay, _] = Object.entries(week).sort((a, b) => {
+    return b[1] - a[1];
+  })[0];
+  return bestDay;
+}
+
 function calculateStatistics(transactions: Transaction[]): Statistics {
   return {
     total: calculateTotal(transactions),
-    creditCard: calculateByFilter<keyof FormOfPayment>(
+    creditCard: countByFilter<keyof FormOfPayment>(
       transactions,
       "formaPagamento",
       "Cartão de Crédito"
     ),
-    billet: calculateByFilter<keyof FormOfPayment>(
+    billet: countByFilter<keyof FormOfPayment>(
       transactions,
       "formaPagamento",
       "Boleto"
     ),
-    paid: calculateByFilter<keyof PaymentStatus>(
-      transactions,
-      "status",
-      "Paga"
-    ),
-    refusedByCreditCard: calculateByFilter<keyof PaymentStatus>(
+    paid: countByFilter<keyof PaymentStatus>(transactions, "status", "Paga"),
+    refusedByCreditCard: countByFilter<keyof PaymentStatus>(
       transactions,
       "status",
       "Recusada pela operadora de cartão"
     ),
-    awaitingPayment: calculateByFilter<keyof PaymentStatus>(
+    awaitingPayment: countByFilter<keyof PaymentStatus>(
       transactions,
       "status",
       "Aguardando pagamento"
     ),
-    reversed: calculateByFilter<keyof PaymentStatus>(
+    reversed: countByFilter<keyof PaymentStatus>(
       transactions,
       "status",
       "Estornada"
     ),
-    bestSellDay: 0,
+    bestSellDay: getBestSalesDay(transactions),
   };
 }
 
